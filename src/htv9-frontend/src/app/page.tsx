3f38c6a9e4eb04abba660ec904c168b164987cfe
Evan Zhao <57083990+Evang264@users.noreply.github.com>
2024-10-05 09:56:37 -0400

    Delete non-existent Dashboard reference

--- src/htv9-frontend/src/app/page.tsx
+++ src/htv9-frontend/src/app/page.tsx
@@ -1,11 +1,9 @@
-import Image from "next/image";
-import Dashboard from "./pages/dashboard";
+// import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] text-3xl items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <h1>Blockchain</h1>
-        <Dashboard />
     </div>
   );
 }
