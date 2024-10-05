380414c90ebd1d77121a77af017b60327eef5014
fayaz-rafin <fayaz.rafin@gmail.com>
2024-10-05 11:14:04 -0400

    added carousel to the homepage

--- src/htv9-frontend/src/app/page.tsx
+++ src/htv9-frontend/src/app/page.tsx
@@ -1,9 +1,21 @@
-// import Image from "next/image";
-
+import Image from "next/image";
+import "../app/globals.css";
+import Carousel from "./components/carousel";
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] text-3xl items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1>Blockchain</h1>
+    <div className="">
+      <div className="w-60 h-10 text-black text-xl font-normal font-['Jersey 10'] mb-2 p-2">VeriThread</div>
+
+            <div className="p-2 text-black text-2xl font-normal font-['Departure Mono']"
+        >Welcome back Sammy</div>
+      <div className="p-2 text-black text-2xl font-family-DepartureMono font-normal"
+      >Welcome back Sammy</div>
+      <div className="p-2 text-black text-2xl font-normal font-['Departure Mono']"
+      >Welcome back Sammy</div>
+
+      <Carousel />
+      <button className="btn btn-neutral">Go to collection</button>
+
     </div>
   );
 }
